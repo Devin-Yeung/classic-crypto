fb9a303d17d340a3c7772ddb849cf2f8ae9ba83e
Devin Yeung <devin.cg.yeung.dev@outlook.com>
2024-04-23 14:56:03 +0800

    feat: add more utils to `State`

--- src/aes/state.rs
+++ src/aes/state.rs
@@ -58,4 +58,42 @@ impl State {
         }
         builder.build().with(Style::modern()).to_string()
     }
+
+    pub fn add_rk(&mut self, rk: &[u32]) {
+        debug_assert!(rk.len() == 4);
+        for col in 0..4 {
+            let rk_col = rk[col].to_be_bytes();
+            for row in 0..4 {
+                self.set(row, col, self.get(row, col) ^ rk_col[row]);
+            }
+        }
+    }
+
+    pub fn sub_bytes(&mut self) {
+        for byte in self.bytes.iter_mut() {
+            *byte = crate::aes::lut::SBOX[*byte as usize];
+        }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    #[test]
+    fn test_add_rk() {
+        // 00112233445566778899aabbccddeeff
+        let data: [u8; 16] = [
+            0x00, 0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77, 0x88, 0x99, 0xaa, 0xbb, 0xcc, 0xdd,
+            0xee, 0xff,
+        ];
+        let rk = [0x00010203, 0x04050607, 0x08090a0b, 0x0c0d0e0f];
+        let mut state = super::State::from_raw(data);
+        state.add_rk(&rk);
+        assert_eq!(
+            state.bytes,
+            [
+                0x00, 0x10, 0x20, 0x30, 0x40, 0x50, 0x60, 0x70, 0x80, 0x90, 0xa0, 0xb0, 0xc0, 0xd0,
+                0xe0, 0xf0
+            ]
+        )
+    }
 }
